62e3b094ba6a2da20df468d015346a4f0540bfd3
Maikel van Dijke <maikelvdijke@gmail.com>
2020-01-31 11:52:00 +0100

    init

---
+++ home.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<head>
+    <title>On the fly</title>
+    <link rel="stylesheet" href="vliegtuigje.css">
+</head>
+<body>
+<ul>
+    <li><a class="active" href="#home">Home</a></li>
+    <li><a href="https://www.instagram.com/esmee.px_/">Test 1</a></li>
+    <li><a href="https://www.instagram.com/pinda_robin/">Test 2</a></li>
+    <li><a href="https://www.instagram.com/maikelvand/">Test 3</a></li>
+</ul>
+
+<div style="margin-left:25%;padding:1px 16px;height:1000px;">
+    <h2>Vliegtuigjes</h2>
+    <h3>Doneer gratis 5 euro, alvast bedankt</h3>
+    <p>Hier komen de mooiste plaatjes van Maikel's vliegtuigje</p>
+</div>
+
+</body>
+</html>
